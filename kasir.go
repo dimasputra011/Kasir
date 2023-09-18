c34ab2e677c42d0fe60567fda9f7e1382f63b17c
dimasputra011 <dimasputramawardi240@gmail.com>
2023-09-18 21:54:56 +0700

    Tugas 1

---
+++ kasir.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Struktur data untuk barang
+type Barang struct {
+	Nama  string
+	Harga float64
+	Stok  int
+}
+
+func main() {
+	// Membuat daftar barang
+	daftarBarang := []Barang{
+		{"Barang 1", 10.0, 100},
+		{"Barang 2", 20.0, 50},
+		{"Barang 3", 30.0, 30},
+	}
+
+	// Menampilkan daftar barang
+	fmt.Println("Daftar Barang yang Tersedia:")
+	for i, barang := range daftarBarang {
+		fmt.Printf("%d. %s (Harga: %.2f, Stok: %d)\n", i+1, barang.Nama, barang.Harga, barang.Stok)
+	}
+
+	// Inisialisasi variabel total dan keranjang belanja
+	var total float64
+	keranjangBelanja := make(map[string]int)
+
+	// Inputan pengguna
+	for {
+		var pilihan int
+		var jumlah int
+
+		fmt.Print("Pilih barang (nomor) atau ketik 0 untuk selesai belanja: ")
+		fmt.Scan(&pilihan)
+
+		if pilihan == 0 {
+			break
+		}
+
+		if pilihan < 1 || pilihan > len(daftarBarang) {
+			fmt.Println("Pilihan tidak valid.")
+			continue
+		}
+
+		fmt.Print("Jumlah barang yang dibeli: ")
+		fmt.Scan(&jumlah)
+
+		barang := daftarBarang[pilihan-1]
+
+		if jumlah > barang.Stok {
+			fmt.Println("Stok tidak mencukupi.")
+			continue
+		}
+
+		// Menambahkan barang ke keranjang belanja
+		keranjangBelanja[barang.Nama] += jumlah
+
+		// Mengurangi stok barang
+		daftarBarang[pilihan-1].Stok -= jumlah
+
+		// Menghitung total
+		total += barang.Harga * float64(jumlah)
+	}
+
+	// Menampilkan total dan cetak hasil total
+	fmt.Printf("Total biaya: %.2f\n", total)
+
+	fmt.Println("Rincian Belanja:")
+	for barang, jumlah := range keranjangBelanja {
+		fmt.Printf("%s: %d\n", barang, jumlah)
+	}
+}
